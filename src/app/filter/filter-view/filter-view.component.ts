0aea884a8348020ba8f9377af73183809c8c4967
Roberto Solorio <robertosolorio@hotmail.com>
2023-01-27 08:24:50 -0600

    Style improvements to quick filter

--- src/app/filter/filter-view/filter-view.component.ts
+++ src/app/filter/filter-view/filter-view.component.ts
@@ -3,7 +3,7 @@ import { SideBarHostStateService } from 'src/app/core/components/side-bar-host/s
 import { SideBarStateService } from 'src/app/core/components/side-bar/side-bar-state.service';
 import { IChipSelectionModel } from 'src/app/shared/components/chip-selection/chip-selection-model.interface';
 import { ChipSelectionComponent } from 'src/app/shared/components/chip-selection/chip-selection.component';
-import { CriteriaOperator, ICriteriaValueBaseModel } from 'src/app/shared/models/criteria-base-model.interface';
+import { CriteriaOperator } from 'src/app/shared/models/criteria-base-model.interface';
 import { CriteriaValueBase } from 'src/app/shared/models/criteria-base.class';
 import { QueryModel } from 'src/app/shared/models/query-model.class';
 import { DatabaseService } from 'src/app/shared/services/database/database.service';
@@ -28,7 +28,7 @@ export class FilterViewComponent implements OnInit {
   }
 
   getDisplayName(columnName: string): string {
-    return this.db.displayName(columnName);
+    return this.db.displayName(columnName).toUpperCase();
   }
 
   getValues(columnName: string): string[] {
@@ -46,7 +46,7 @@ export class FilterViewComponent implements OnInit {
   private async openChipSelectionPanel(columnName: string): Promise<void> {
     const availableValues = await this.db.getSongValues(columnName);
     const chipSelectionModel: IChipSelectionModel = {
-      title: this.getDisplayName(columnName),
+      title: this.db.displayName(columnName),
       values: [],
       onOk: values => {
         let criteriaItem = this.model.userCriteria.find(item => item.ColumnName === columnName);
@@ -55,13 +55,17 @@ export class FilterViewComponent implements OnInit {
           this.model.userCriteria.push(criteriaItem);
         }
         criteriaItem.ColumnValues = values.map(value => value.data);
-      }
+      },
+      singleSelect: columnName === 'favorite'
     };
+    const criteriaItem = this.model.userCriteria.find(item => item.ColumnName === columnName);
     for (const value of availableValues) {
+      let selected = criteriaItem && criteriaItem.ColumnValues.includes(value.toString());
       chipSelectionModel.values.push({
         id: value.toString(),
         caption: value.toString(),
-        data: value.toString()
+        data: value.toString(),
+        selected: selected
       });
     }
     this.sidebarHostService.loadComponent(ChipSelectionComponent, chipSelectionModel);
