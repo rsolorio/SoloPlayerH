3a95ecc873f65ad7a9529d0679a40137eacb9f31
Roberto Solorio <robertosolorio@hotmail.com>
2023-02-13 15:02:52 -0600

    Breadcrumb scroll

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CoreComponent } from 'src/app/core/models/core-component.class';
+import { CoreEvent } from 'src/app/core/services/events/events.enum';
 import { EventsService } from 'src/app/core/services/events/events.service';
 import { BreadcrumbEventType } from '../../models/breadcrumbs.enum';
 import { AppEvent } from '../../models/events.enum';
@@ -12,8 +13,11 @@ import { BreadcrumbsStateService } from './breadcrumbs-state.service';
   styleUrls: ['./breadcrumbs.component.scss']
 })
 export class BreadcrumbsComponent extends CoreComponent implements OnInit {
-
+  @ViewChild('breadcrumbsContainer') private breadcrumbsContainer: ElementRef;
   public model: IBreadcrumbModel[];
+  public leftScrollIndicatorVisible = false;
+  public rightScrollIndicatorVisible = false;
+  public containerClass = 'sp-bc-icon-null';
 
   constructor(private breadcrumbService: BreadcrumbsStateService, private events: EventsService) {
     super();
@@ -26,9 +30,16 @@ export class BreadcrumbsComponent extends CoreComponent implements OnInit {
         this.reload();
       }
     });
-  }
 
-  public onContainerScroll(): void {}
+    this.subs.sink = this.events.onEvent(CoreEvent.WindowSizeChanged).subscribe(() => {
+      this.updateScrollIndicators();
+    });
+
+    // Give time to render content
+    setTimeout(() => {
+      this.updateScrollIndicators();
+    });
+  }
 
   public onClick(breadcrumb: IBreadcrumbModel): void {
     if (breadcrumb.last) {
@@ -43,5 +54,92 @@ export class BreadcrumbsComponent extends CoreComponent implements OnInit {
 
   public reload(): void {
     this.model = this.breadcrumbService.getState();
+  }
+
+  public onContainerScroll(): void {
+    this.updateScrollIndicators();
+  }
+
+  public scrollLeft(value: number): void {
+    let position = this.breadcrumbsContainer.nativeElement.scrollLeft as number;
+    // Can be scrolled?
+    if (position > 0) {
+      position -= value;
+      // Do not go below zero
+      if (position >= 0) {
+        this.breadcrumbsContainer.nativeElement.scrollLeft = position;
+      }
+      else {
+        this.breadcrumbsContainer.nativeElement.scrollLeft = 0;
+      }
+    }
+  }
+
+  public scrollToLeft(): void {
+    this.breadcrumbsContainer.nativeElement.scrollLeft = 0;
+  }
+
+  public scrollRight(value: number): void {
+    let position = this.breadcrumbsContainer.nativeElement.scrollLeft as number;
+    const width = this.breadcrumbsContainer.nativeElement.scrollWidth as number;
+    // Can be scrolled?
+    if (position < width) {
+      position += value;
+      // Do not go beyond the available width
+      if (position <= width) {
+        this.breadcrumbsContainer.nativeElement.scrollLeft = position;
+      }
+      else {
+        this.breadcrumbsContainer.nativeElement.scrollLeft = width;
+      }
+    }
+  }
+
+  public scrollToRight(): void {
+    this.breadcrumbsContainer.nativeElement.scrollLeft = this.breadcrumbsContainer.nativeElement.scrollWidth;
+  }
+
+  private updateScrollIndicators(): void {
+    if (this.breadcrumbsContainer && this.breadcrumbsContainer.nativeElement) {
+      const scrollArea = this.breadcrumbsContainer.nativeElement;
+      this.leftScrollIndicatorVisible = scrollArea.scrollLeft > 0;
+      this.rightScrollIndicatorVisible =
+        scrollArea.scrollWidth > scrollArea.offsetWidth && (scrollArea.scrollLeft + scrollArea.offsetWidth) < scrollArea.scrollWidth;
+    }
+    if (this.leftScrollIndicatorVisible) {
+      if (this.rightScrollIndicatorVisible) {
+        this.containerClass = 'sp-bc-icon-both';
+      }
+      else {
+        this.containerClass = 'sp-bc-icon-left';
+      }
+    }
+    else if (this.rightScrollIndicatorVisible) {
+      this.containerClass = 'sp-bc-icon-right';
+    }
+    else {
+      this.containerClass = 'sp-bc-icon-null';
+    }
+  }
+
+  public onContainerWheel(wheelEvent: any): void {
+    if (wheelEvent.type === 'wheel') {
+      if (wheelEvent.deltaY < 0) {
+        // Scrolling up, moving left
+        this.scrollLeft(10);
+      }
+      else if (wheelEvent.deltaY > 0) {
+        // Scrolling down, moving right
+        this.scrollRight(10);
+      }
+    }
+  }
+
+  public onScrollLeftClick(): void {
+    this.scrollToLeft();
+  }
+
+  public onScrollRightClick(): void {
+    this.scrollToRight();
   }
 }
