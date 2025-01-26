de3fd3826670d559da363648fbe992d9bac28081
Roberto Solorio <robertosolorio@hotmail.com>
2025-01-25 21:44:12 -0600

    quiz title and song cache

--- src/app/player/player-quiz/player-quiz.component.ts
+++ src/app/player/player-quiz/player-quiz.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AppActionIcons, AppAttributeIcons, AppFeatureIcons, AppPlayerIcons } from 'src/app/app-icons';
+import { NavbarDisplayMode } from 'src/app/core/components/nav-bar/nav-bar-model.interface';
+import { NavBarStateService } from 'src/app/core/components/nav-bar/nav-bar-state.service';
 import { SideBarHostStateService } from 'src/app/core/components/side-bar-host/side-bar-host-state.service';
 import { IImage, ISelectableValue } from 'src/app/core/models/core.interface';
+import { IconActionArray } from 'src/app/core/models/icon-action-array.class';
 import { LogService } from 'src/app/core/services/log/log.service';
 import { UtilityService } from 'src/app/core/services/utility/utility.service';
 import { MusicImageType } from 'src/app/platform/audio-metadata/audio-metadata.enum';
@@ -15,7 +18,9 @@ import { DatabaseEntitiesService } from 'src/app/shared/services/database/databa
 import { RelatedImageSrc } from 'src/app/shared/services/database/database.seed';
 import { DatabaseService } from 'src/app/shared/services/database/database.service';
 import { HtmlMediaEvent } from 'src/app/shared/services/html-player/html-player.enum';
+import { LocalStorageService } from 'src/app/shared/services/local-storage/local-storage.service';
 import { Not } from 'typeorm';
+import { IQuizCache } from './player-quiz.interface';
 
 @Component({
   selector: 'sp-player-quiz',
@@ -50,6 +55,8 @@ export class PlayerQuizComponent implements OnInit {
   private playTimer = null;
   private isReplacing = false;
   public isPlaying = false;
+  private cache: IQuizCache;
+  private cacheKey = 'QuizCache'
 
   constructor(
     private sidebarHostService: SideBarHostStateService,
@@ -57,10 +64,32 @@ export class PlayerQuizComponent implements OnInit {
     private entities: DatabaseEntitiesService,
     private utility: UtilityService,
     private imageService: ImageService,
+    private navbarService: NavBarStateService,
+    private localStorage: LocalStorageService,
     private log: LogService) { }
 
   ngOnInit(): void {
     this.subscribeToAudioEvents();
+    this.initializeNavBar();
+    const cacheData = this.localStorage.getByKey<IQuizCache>(this.cacheKey);
+    if (cacheData) {
+      this.cache = cacheData;
+    }
+    else {
+      this.cache = {
+        songs: []
+      };
+    }
+  }
+
+  public initializeNavBar() {
+    const navbar = this.navbarService.getState();
+    navbar.title = 'Quiz';
+    navbar.mode = NavbarDisplayMode.Title;
+    navbar.menuList = [{
+      icon: AppActionIcons.Delete,
+      caption: 'Clear History'
+    }];
   }
 
   public onLanguageEditClick() {
@@ -174,8 +203,7 @@ export class PlayerQuizComponent implements OnInit {
     this.sidebarHostService.loadContent(chipSelectionModel);
   }
 
-  private async find() {
-    this.songInfoVisible = false;
+  private async findSong(): Promise<SongExtendedViewEntity> {
     const criteria = new Criteria();
     criteria.paging.pageSize = 1;
     criteria.random = true;
@@ -195,53 +223,69 @@ export class PlayerQuizComponent implements OnInit {
     // Ignore "bad" music
     criteria.searchCriteria.push(new CriteriaItem('rating', 2, CriteriaComparison.GreaterThan));
     const songs = await this.db.getList(SongExtendedViewEntity, criteria);
-
+    // Get a song that is not part of the cache
     if (songs.length) {
-      const song = songs[0];
-      let image: IImage;
-      const songImages = await RelatedImageEntity.findBy({ relatedId: song.id });
-      if (songImages.length) {
-        image = await this.imageService.getImageFromSource(songImages[0]);
-      }
-      else {
-        const albumImages = await RelatedImageEntity.findBy({ relatedId: song.primaryAlbumId, imageType: Not(MusicImageType.FrontAnimated) });
-        if (albumImages.length) {
-          image = await this.imageService.getImageFromSource(albumImages[0]);
+      for (const song of songs) {
+        const matchingSong = this.cache.songs.find(songId => songId === song.id);
+        if (!matchingSong) {
+          this.cache.songs.push(song.id);
+          this.localStorage.setByKey(this.cacheKey, this.cache);
+          return song;
         }
       }
-      if (image) {
-        this.imageSrc = image.src;
-      }
-      const artists = await (await this.entities.prepareScrobbleRequest(song.id)).artistName;
-      this.log.clearConsole();
-      this.log.table('Song found:', { tabular: {
-        name: song.name,
-        cleanName: song.cleanName,
-        albumArtist: song.primaryArtistName,
-        artist: artists,
-        album: song.primaryAlbumStylized,
-        year: song.releaseYear
-      } });
-      this.filePath = song.filePath;
-      this.songName = song.cleanName;
-      this.artistName = artists;
-      this.albumName = song.primaryAlbumStylized;
-      this.releaseYear = song.releaseYear;
-      this.languageName = song.language;
-      this.genre = song.genre;
-      this.decade = song.releaseDecade;
-      this.elapsedTime = 0;
-      this.remainingTime = song.seconds;
+    }
+    return null;
+  }
 
-      if (this.isPlaying) {
-        this.pauseAndReplaceAudioSource();
-      }
-      else {
-        this.replaceAudioSource();
-      }
+  private async find() {
+    this.songInfoVisible = false;
+    const song = await this.findSong();
+
+    if (!song) {
+      // TODO: song not found message
+      return;
+    }
+
+    let image: IImage;
+    const songImages = await RelatedImageEntity.findBy({ relatedId: song.id });
+    if (songImages.length) {
+      image = await this.imageService.getImageFromSource(songImages[0]);
     }
     else {
-      // TODO: song not found message
+      const albumImages = await RelatedImageEntity.findBy({ relatedId: song.primaryAlbumId, imageType: Not(MusicImageType.FrontAnimated) });
+      if (albumImages.length) {
+        image = await this.imageService.getImageFromSource(albumImages[0]);
+      }
+    }
+    if (image) {
+      this.imageSrc = image.src;
+    }
+    const artists = await (await this.entities.prepareScrobbleRequest(song.id)).artistName;
+    this.log.clearConsole();
+    this.log.table('Song found:', { tabular: {
+      name: song.name,
+      cleanName: song.cleanName,
+      albumArtist: song.primaryArtistName,
+      artist: artists,
+      album: song.primaryAlbumStylized,
+      year: song.releaseYear
+    } });
+    this.filePath = song.filePath;
+    this.songName = song.cleanName;
+    this.artistName = artists;
+    this.albumName = song.primaryAlbumStylized;
+    this.releaseYear = song.releaseYear;
+    this.languageName = song.language;
+    this.genre = song.genre;
+    this.decade = song.releaseDecade;
+    this.elapsedTime = 0;
+    this.remainingTime = song.seconds;
+
+    if (this.isPlaying) {
+      this.pauseAndReplaceAudioSource();
+    }
+    else {
+      this.replaceAudioSource();
     }
   }
 
---
+++ src/app/player/player-quiz/player-quiz.interface.ts
@@ -0,0 +1,3 @@
+export interface IQuizCache {
+  songs: string[];
+}
